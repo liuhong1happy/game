93f849d303291e4528777964d9c7fec14d9577f3
liuhong <liuhong@163.com>
2018-05-23 14:08:46 +0800

    更新

--- src/scenes/history.js
+++ src/scenes/history.js
@@ -26,7 +26,7 @@ export default {
         if(findIndex !== -1) {
             let scene = this.scenes[findIndex].scene;
             // 卸载当前场景
-            this.current.scene.Unload(); 
+            // this.current.scene.Unload(); 
             // 初始化新的场景
             scene.Init(this.current.scene.canvas);
             // 保存新的场景
@@ -47,13 +47,18 @@ export default {
         var findIndex = this.scenes.findIndex(scene=> scene.path === path );
         if(findIndex !== -1) {
             let scene = this.scenes[findIndex].scene;
+            // 卸载当前场景
+            this.current.scene.Unload(); 
+            // 初始化新的场景
             scene.Init(this.current.scene.canvas);
+            // 保存新的场景
             this.current = {
                 path,
                 params,
                 scene,
             }
             this.current.scene.params = this.current.params;
+            // 将新的场景放入历史堆栈中
             this.history = [this.current];
             return this.current;
         } else {
@@ -62,6 +67,9 @@ export default {
     },
     pop() {
         if(this.history.length>=1) {
+            // 卸载当前场景
+            this.current.scene.Unload(); 
+            // 回退到上一级
             return this.history.pop()
         } else {
             return false;
